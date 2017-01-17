3a63439da72bbe2191ea598158a13f690e2019f3
Mike Daly <mdaly@l4digital.com>
2017-01-17 13:53:14 -0800

    Adding second video tag.

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,17 @@
 (function () {
 	try {
-		(function () {
-			var node = document.getElementsByTagName('body')[0];
-			var newNode = document.createElement('div');
-			newNode.innerHTML = '<div id="root"> <video></video></div>';
-			node.appendChild(newNode);
-		})();
-		(function () {
-			debugger;
-			var node = document.getElementById(root);
-			debugger;
-			while (node.firstChild) {
-				debugger;
-				node.removeChild(node.firstChild);
-			}
-		})();
-		(function () {
-			var node = document.getElementById(root);
-			var newNode = document.createElement('div');
-			newNode.innerHTML = '<div> <span></span></div>';
-			node.appendChild(newNode);
-		})();
+		let node = document.getElementById('main-video');
+		let bounds = node.getBoundingClientRect();
+		return {
+			'top': bounds.top,
+			'left': bounds.left,
+			'right': bounds.right,
+			'bottom': bounds.bottom,
+			'width': bounds.width,
+			'height': bounds.height
+		};
 		;
-		return true;
 	} catch (err) {
-		return err.message;
+		return {'error': err.message};
 	}
 })();
